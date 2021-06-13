f9bfbce3b4c8fa502c3c44333d35f4e0c40fa62f
tantalog <kushnerovanton@gmail.com>
2021-06-13 19:02:48 +0300

    feat: exercise 1 complete

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -3,6 +3,16 @@ import Foundation
 class StockMaximize {
 
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        var profit = 0
+        for i in 0..<prices.count {
+            var maxMargin = 0
+            for j in i+1 ..< prices.count {
+                if prices[j] - prices[i] > maxMargin {
+                    maxMargin = prices[j] - prices[i]
+                }
+            }
+            profit += maxMargin
+        }
+        return profit
     }
 }
