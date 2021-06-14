afada6c4bdb695a18083e6f9af65064d7d4fb423
tantalog <kushnerovanton@gmail.com>
2021-06-14 20:16:07 +0300

    feat: Exercise 2 complete

--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -3,6 +3,36 @@ import UIKit
 class MessageDecryptor: NSObject {
     
     func decryptMessage(_ message: String) -> String {
+        guard message.count >= 1 && message.count <= 60 else {
+            return ""
+        }
+        var output = message
+        
+        while let range = output.range(of: #"[0-9]+\[[a-z]+\]"#, options: .regularExpression){
+            var index = range.lowerBound
+            var wordMultiplier = ""
+            var word = ""
+            
+            while index != range.upperBound {
+                if output[index].isNumber {
+                    wordMultiplier.append(output[index])
+                } else if output[index].isLetter {
+                    word.append(output[index])
+                }
+                index = output.index(after: index)
+            }
+            
+            let multiplier = Int(wordMultiplier) ?? 0
+            if multiplier < 1 || multiplier > 300  {
+                return ""
+            }
+            output.removeSubrange(range)
+            output.insert(contentsOf: String.init(repeating: word, count: multiplier), at: range.lowerBound)
+        }
+        
+        if let range = output.range(of: #"[a-z]+"#, options: .regularExpression) {
+            return String(output[range.lowerBound ... output.index(before: (range.upperBound))])
+        }
         return ""
     }
 }
